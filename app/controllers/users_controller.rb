9702a7bdd9f261f88d10070584271f2932db7842
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-12 20:55:58 -0500

    add first pass of users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,15 @@ class UsersController < ApplicationController
   # end
 
   # # POST /users
-  # def create
-  #   @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-  #   if @user.save
-  #     render json: @user, status: :created, location: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
   # # PATCH/PUT /users/1
   # def update
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   # end
 
   # # Only allow a list of trusted parameters through.
-  # def user_params
-  #   params.require(:user).permit(:username, :password_digest)
-  # end
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
