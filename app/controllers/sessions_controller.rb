49a9b29790bb252416cf3351dd0a30f2dba655aa
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-12 21:50:26 -0500

    remove session store

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
-  def new
+  def create
+    user = User
+      .find_by(username: params["session"]["username"])
+      .try(:authenticate, params["session"]["password"])
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user,
+        only: [:username],
+      }
+    else
+      render json: { status: 401 }
+    end
+  end
+
+  def destroy
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,4 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  def create
-  end
-
-  def destroy
-  end
 end
