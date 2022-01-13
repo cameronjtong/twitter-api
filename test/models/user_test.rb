a3ca03629cf29dc835f3df4f3976eb9953ef6d3d
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-12 19:54:55 -0500

    add user validations and testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: 'camjtong')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'username must be present' do
+    @user.username = " "
+
+    assert_not @user.valid?
+  end
+
+  test 'username must not be too long' do
+    @user.username = "a"*51
+
+    assert_not @user.valid?
+  end
+
+  test 'usernames are unique independant of case' do
+    duplicate_user = @user.dup
+    duplicate_user.username = @user.username.upcase
+
+    @user.save
+
+    assert_not duplicate_user.valid?
+  end
 end
