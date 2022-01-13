72d699956f06bde75e5cef2a14cb57ad15caae4e
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-13 18:22:45 -0500

    create message and conversation models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
 
-  # # GET /users
   def index
     users = User.all
 
@@ -19,19 +18,16 @@ class UsersController < ApplicationController
       log_in(user)
       render json: user, excpet: [:password_digest], status: :created, location: user
     else
-      render json: {errors: user.errors}
+      render json: { errors: user.errors }
     end
   end
 
-
   private
 
-  # # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_113_003_608) do
+ActiveRecord::Schema.define(version: 20_220_113_231_859) do
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
