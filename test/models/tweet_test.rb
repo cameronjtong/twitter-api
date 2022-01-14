91368474a556f9d95213eaa99c0959eafa095781
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-13 21:47:35 -0500

    finish user micropost association

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class TweetTest < ActiveSupport::TestCase
   def setup
     @user = users(:cam)
-    @tweet = Tweet.new(content: 'Lorem text', user_id: @user.id)
+    @tweet = @user.tweets.build(content: 'Lorem text')
   end
 
   test 'tweets must have content' do
